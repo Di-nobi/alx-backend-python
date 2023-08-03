8e480fefd30d63329cb64a8562e580d48dd9fcdb
Dinobi <architectstan01@gmail.com>
2023-08-03 17:37:58 +0100

    Python Backend Annotation

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''A function that takes a float as an argument and returns a function
+multiplies a float by by multiplier'''
+
+from typing import Callable
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''A function that takes a float as an argument and returns a function
+    multiplies a float by by multiplier'''
+    return lambda x: x * multiplier
