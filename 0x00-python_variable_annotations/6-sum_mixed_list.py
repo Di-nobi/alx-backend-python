bf1d65d07d72f6efd518364521a9888526fe224a
Dinobi <architectstan01@gmail.com>
2023-08-03 23:05:09 +0100

    Python Backend Annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-'''A function that takes a list of integers and floats
-and returns the sum as a float'''
 
+'''A function that takes a list of integers and floats
+and returns the sum as a float
+'''
 from typing import List, Union
+
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     '''A function that takes a list of integers and floats
     and returns the sum as a float'''
