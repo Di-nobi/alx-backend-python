9b35e2298bdcd3b928e21a881bf468bd6c2d9aad
Dinobi <architectstan01@gmail.com>
2023-08-03 17:00:53 +0100

    Python Backend Annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A function which takes a list of floats as 
+argument and returns their sum as a float'''
+def sum_list(input_list: list[float]) -> float:
+    '''A function which takes a list of floats as 
+argument and returns their sum as a float'''
+    return float(sum(input_list))
