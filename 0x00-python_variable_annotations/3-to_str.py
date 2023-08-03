af0f5955967deaf112aa8d1c1846deedcdab88e6
Dinobi <architectstan01@gmail.com>
2023-08-03 16:46:48 +0100

    Python Backend Annotation

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,4 +2,5 @@
 ''' A type annotated function that takes a float as argument and returns the string'''
 
 def to_str(n: float) -> str:
+    '''Takes in a float and converts it to an integer'''
     return str(n)
