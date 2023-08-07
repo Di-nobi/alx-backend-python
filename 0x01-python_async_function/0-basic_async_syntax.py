6c5d7788fa30173364473489d648b4d9fcb0c82e
Dinobi <architectstan01@gmail.com>
2023-08-07 15:13:00 +0100

    Python Backend ASYNC/AWAIT

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''A program that takes in an integers and wait for its delay time'''
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    ''' Awaits for a random digit'''
+    LetsWait = max_delay * random.random()
+    await asyncio.sleep(LetsWait)
+    return LetsWait
