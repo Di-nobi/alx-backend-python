64c2c35f213e6f3d6a3347c4448716e28516ca67
Dinobi <architectstan01@gmail.com>
2023-11-07 13:06:11 +0100

    python tasks

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,8 +4,10 @@ import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     '''Async_comprehension to be executed 4 times'''
     start = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(), async_comprehension(), async_comprehension())
     return time.time() - start
