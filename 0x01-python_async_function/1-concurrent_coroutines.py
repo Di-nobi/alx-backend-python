b185aa9b72700e998f237ad19a42ca766db5deab
Dinobi <architectstan01@gmail.com>
2023-11-07 12:55:28 +0100

    python async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Executing coroutines at the same time with async"""
+"""
+Executing coroutines at the same time with async"""
 from typing import List
-import random
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    '''Executing multiple coroutines at the same time with async'''
+    """Gets the lists of the delays (floats value)"""
     mergelist = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(mergelist)
